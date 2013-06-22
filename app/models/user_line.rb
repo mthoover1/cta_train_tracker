79a3ee77507f5e94f34e16317dff74bc6bae80b5
Matthew Hoover <mthoover1@gmail.com>
2013-06-22 14:08:44 -0500

    uniqueness validation on user stations and lines

--- app/models/user_line.rb
+++ app/models/user_line.rb
@@ -1,4 +1,6 @@
 class UserLine < ActiveRecord::Base
   belongs_to :user
   belongs_to :line
+
+  validates_uniqueness_of :user_id, :scope => :line_id
 end
--- app/models/user_station.rb
+++ app/models/user_station.rb
@@ -1,4 +1,6 @@
 class UserStation < ActiveRecord::Base
   belongs_to :user
   belongs_to :station
+
+  validates_uniqueness_of :user_id, :scope => :station_id
 end
